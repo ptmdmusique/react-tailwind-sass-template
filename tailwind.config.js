98129d4a5dcc3c1d39e625e8ec1fc913c1de6744
ptmdmusique <ptmdmusic@gmail.com>
2021-03-22 12:31:32 -0700

    update lib and add web vital

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,25 @@ module.exports = {
   prefix: "",
   important: false,
   separator: ":",
+  corePlugins: {},
+  // purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
   theme: {
     screens: {
       sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
       md: "768px",
+      // => @media (min-width: 768px) { ... }
+
       lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
       xl: "1280px",
-      "2xl": "2160px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
   },
-  corePlugins: {},
   plugins: [require("tailwindcss"), require("autoprefixer")],
 };
